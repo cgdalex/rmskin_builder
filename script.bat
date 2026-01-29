0910e16499254c3b3d1f0483c2b4d14e1fb3a4eb
Alex <garciaalex0827@gmail.com>
2026-01-29 14:13:33 -0800

    Enhance script.bat with detailed build process information and success messages

--- script.bat
+++ script.bat
@@ -1,11 +1,25 @@
 @echo off
+
+echo --------------------------------
+echo cgdalex RMSkin Builder v1.0.0
+echo --------------------------------
+echo Author: Alex Cortes
+echo GitHub: https://github.com/cgdalex
+echo Email: cgdalex@gmail.com
+echo --------------------------------
+
+
+echo --------------------------------
 echo Please include .gif files in the ./gifs/ directory for proper RMSkin functionality.
+echo --------------------------------
 if not exist gifs (
     echo Warning: ./gifs/ directory not found. Please create it and add .gif files as needed.
+    echo --------------------------------
     exit
 )
 
 echo Starting RMSkin build process...
+echo --------------------------------
 @REM  Setup Virtual Environment
 if not exist venv (
     echo Setting up virtual environment...
@@ -24,8 +38,10 @@ if not exist venv (
 @REM Run the RMSkin builder script
 echo Starting RMSkin builder script...
 python rmskin_builder.py
-echo RMSkin build process complete.
-
-echo 
-
-echo Please check the templates directory for your RMSkin folder.
+echo --------------------------------
+echo SUCCESS: RMSkin build process completed.
+echo --------------------------------
+echo Please check the \output directory for your RMSkin folder.
+echo --------------------------------
+pause
+exit
